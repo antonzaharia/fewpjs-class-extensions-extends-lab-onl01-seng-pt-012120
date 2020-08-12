07578f165f48ca1a1b3aa94e9ba5dd512bcf7e86
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-12 16:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Polygon {
 class Triangle extends Polygon {
   get isValid() {
     if((this.array[0] + this.array[1]) > this.array[2]) {
-      return false
+      return true
     }
   }
 }
